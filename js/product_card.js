135777a6a4e85da79200d87b74977c581b6d7737
Muhammed Adhil p <167611139+adhilbathali@users.noreply.github.com>
2024-07-27 00:49:47 +0530

    add search products

--- js/product_card.js
+++ js/product_card.js
@@ -1,15 +1,20 @@
 const productCards = document.getElementsByClassName("product_card");
+const cardContainer = document.querySelector("[search-bar]");
+
 
 for (let index = 0; index < productCards.length; index++) {
     productCards[index].addEventListener('click', 
         function(){
             const imageSrc = productCards[index].querySelector('img').src;
             const productName = productCards[index].querySelector('h1').textContent;
-            const currentCard = {
-                "heading" : productName,
-                "src" : imageSrc,
-            }
-            sessionStorage.setItem('currentCard', JSON.stringify(currentCard));
+            var productClasses = productCards[index].getAttribute('class');
+            productClasses = productClasses.split(' ');
+            const category = productClasses[1];
+            const subCategory = productClasses[2];
+            sessionStorage.setItem("imageSrc", imageSrc);
+            sessionStorage.setItem("heading", productName);
+            sessionStorage.setItem("category", category);
+            sessionStorage.setItem("subCategory", subCategory);
         }
     )
 }
--- js/product_page.js
+++
@@ -1,8 +0,0 @@
-const currentCard = sessionStorage.getItem('currentCard');
-const productData = JSON.parse(currentCard);
-
-const mainHeading = productData.heading;
-const imageSrc = productData.src;
-
-document.getElementById('product-page-heading').textContent = mainHeading;
-document.getElementById('product-page-image').src = imageSrc;   
