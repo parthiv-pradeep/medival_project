3aec784dabc1af2f2fb294d804ca93c7e7a95d25
Muhammed Adhil p <167611139+adhilbathali@users.noreply.github.com>
2024-07-30 22:10:15 +0530

    add fast selling products

--- js/event_delegation.js
+++ js/event_delegation.js
@@ -1,13 +1,13 @@
-const productConatainer = document.querySelector("[product-cards]");
+const productContainer = document.querySelector("[product-cards]");
 var productName;
-console.log(productConatainer);
 
 
-productConatainer.addEventListener('click',
+productContainer.addEventListener('click',
     function(event){
         const card = event.target.closest('.product_card');
         if(card){
             productName = card.querySelector(".card-name").textContent;
+            sessionStorage.setItem('name',productName);
             fetch("./json/products.json").then(res => res.json()).then(data => {
                 const cardDetails = data.find(product => product.name === productName);
                 if(cardDetails){
