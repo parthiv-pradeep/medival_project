8cfce624c1112175cd867fa9196b32fc9128d1ab
Muhammed Adhil p <167611139+adhilbathali@users.noreply.github.com>
2024-08-01 19:18:20 +0530

    Add email toggle

--- js/whatsapp_toggle.js
+++ js/whatsapp_toggle.js
@@ -1,5 +1,10 @@
 const whatsappIcon = document.getElementById('whatsapp-icon');
 const chatBox = document.getElementById('chat-box');
+
+const emailIcon = document.getElementById('email-icon');
+const inbox = document.getElementById('email-inbox');
+
+
 whatsappIcon.addEventListener('click', function() {
     if (chatBox.style.display === 'block'){
         chatBox.style.display = 'none';
@@ -10,3 +15,16 @@ whatsappIcon.addEventListener('click', function() {
         whatsappIcon.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     }
 });
+
+
+emailIcon.addEventListener('click', function(){
+    if(inbox.style.display === 'block'){
+        inbox.style.display = 'none';
+        emailIcon.innerHTML = '<i class="fa-regular fa-envelope"></i>';
+    }
+    else{
+        inbox.style.display = 'block';
+        emailIcon.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    }
+})
+
